d0ebdd2bcd878805c975f96d837b271cf0e3f97b
Rasmus Jonssønn Skramstad <rasmus.skramstad@gmail.com>
2023-02-09 10:22:04 +0100

    inital commit

--- server-multithreaded.py
+++ server-multithreaded.py
@@ -1,11 +1,13 @@
-# Currently, your web server handles only one HTTP request at a time. You should implement a multithreaded server
-# that is capable of serving multiple requests simultaneously. Using threading, first create a main thread in which
-# your modified server listens for clients at a fixed port. When it receives  a TCP connection request from a client,
-# it will set up the TCP connection through another port and services the client request in a separate thread. There
-# will be a separate TCP connection in a separate thread for each request/response pair
+# A server should keep track of the total number of clients, allow clients to send messages and broadcast
+# everyone. Below are some key functions you must implement:
+# • You should implement a function named broadcast to notify everyone when a client joins (except
+# the client who joined).
+# • You should also implement a function called game where it will allow two clients to play rock,
+# paper, scissors game.
 
 import _thread as thread
 import socket
+import time
 
 # Define the host and port
 host, port = "127.0.0.1", 9090
@@ -30,6 +32,7 @@ def handleclient(clientSocket, clientIP):
     print("Started new thread for client" + str(clientIP) + "\n_______________________________________________________")
     # Get the request
     clientRequest = clientSocket.recv(4096).decode()
+    print(clientRequest)
     # Encode the response
     response = "Received request: " + clientRequest
     response = response.encode()
@@ -39,11 +42,42 @@ def handleclient(clientSocket, clientIP):
     clientSocket.close()
 
 
+def broadcast(clientSocket, clientIP):
+    print("Started new thread for client" + str(clientIP) + "\n_______________________________________________________")
+    # Get the request
+    clientRequest = clientSocket.recv(4096).decode()
+    print(clientRequest)
+    # Encode the response
+    response = "Received request: " + clientRequest
+    response = response.encode()
+    # Send the response
+    clientSocket.sendall(response)
+
+
+# Check if any clients have disconnected
+def checkForConnection():
+    # Sleep for 10 seconds
+    time.sleep(10)
+    for client in clients:
+        try:
+            client.sendall("Hello".encode())
+        except socket.error:
+            print("Client " + str(client) + " disconnected from the server")
+            clients.remove(client)
+
+
+# List of clients
+clients = []
 # This loop will run forever, accepting connections and serving creating a new thread for each connection
 while True:
     # Accept a connections
     clientSocket, addr = serverSocket.accept()
+    # Add the client to the list of clients
+    clients.append(clientSocket)
+    print(str(clientSocket) + " connected to the server" + str(addr))
     thread.start_new_thread(handleclient, (clientSocket, addr))
+    # Start a new thread to check for disconnected clients
+    thread.start_new_thread(checkForConnection, ())
 
 # Close the socket
 serverSocket.close()
