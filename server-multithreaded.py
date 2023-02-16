3e4cef2e037bd73cc9d62b54e348322661803295
Rasmus Jonssønn Skramstad <rasmus.skramstad@gmail.com>
2023-02-16 14:15:28 +0100

    Lagde lobby for spillere, får også hentet meldingen de skriver

--- server-multithreaded.py
+++ server-multithreaded.py
@@ -1,8 +1,8 @@
-# A server should keep track of the total number of clients, allow clients to send messages and broadcast
+# A server should keep track of the total number of activeSockets, allow activeSockets to send messages and broadcast
 # everyone. Below are some key functions you must implement:
 # • You should implement a function named broadcast to notify everyone when a client joins (except
 # the client who joined).
-# • You should also implement a function called game where it will allow two clients to play rock,
+# • You should also implement a function called game where it will allow two activeSockets to play rock,
 # paper, scissors game.
 
 import _thread as thread
@@ -15,7 +15,7 @@ host, port = "127.0.0.1", 9091
 # Create a socket
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-linje = "\n_______________________________________________________"
+line = "\n_______________________________________________________"
 
 # Try to bind the socket to the host and port, if it fails close the program
 try:
@@ -30,18 +30,18 @@ serverSocket.listen()
 
 
 def message(socketNumber, text):
-    global clients
+    global activeSockets
     socketNumber = str(socketNumber)
-    # Send message to all clients if * is used
+    # Send message to all activeSockets if * is used
     if socketNumber == "*":
-        for client in clients:
+        for client in activeSockets:
             client.send(text.encode())
-        print("Sending message to all clients –> " + text)
+        print("Sending message to all activeSockets –> " + text)
     # Send message to specific client if a number is used
     else:
         try:
             socketNumber = int(socketNumber)
-            client = clients[int(socketNumber)]
+            client = activeSockets[int(socketNumber)]
             client.send(text.encode())
             print("Sending message to " + str(socketNumber) + " –> " + text)
         except ValueError:
@@ -52,17 +52,17 @@ def helpClient():
     helptext = "Commands: \n"
     helptext += "––> help - Show this help screen \n"
     helptext += "––> exit - Exit the server\n"
-    helptext += "––> list - List all connected clients\n"
-    helptext += "––> message id message - Send a message to a specific client or use * for all clients\n"
+    helptext += "––> list - List all connected activeSockets\n"
+    helptext += "––> message id message - Send a message to a specific client or use * for all activeSockets\n"
     helptext += "––> log - Get the request log\n"
-    helptext += "––> game - Start a game with a client \n" + linje
+    helptext += "––> game - Start a game with a client \n" + line
     return helptext
 
 
 def listOfClients(clientSocket):
-    clientlist = "Connected clients:  \n"
+    clientlist = "Connected activeSockets:  \n"
     i = 0
-    for client in clients:
+    for client in activeSockets:
         if client == clientSocket:
             clientlist += "Your clientID is " + str(i)
         else:
@@ -74,14 +74,14 @@ def listOfClients(clientSocket):
 
 def getClientId(clientSocket):
     i = 0
-    for client in clients:
+    for client in activeSockets:
         if client == clientSocket:
             return i
         i += 1
 
 
 def startGame(firstClientNumber, secondClientNumber):
-    global clients
+    global activeSockets
     global gameStarted
 
     # Save the clientID as an integer
@@ -92,10 +92,10 @@ def startGame(firstClientNumber, secondClientNumber):
     message(secondClientNumber,
             str(firstClientNumber) + " wants to play a game with you. Type 'yes' to accept or 'no' to decline")
 
-    # Get the socket number from the clients
+    # Get the socket number from the activeSockets
     try:
-        client1Socket = clients[firstClientNumber]
-        client2Socket = clients[secondClientNumber]
+        client1Socket = activeSockets[firstClientNumber]
+        client2Socket = activeSockets[secondClientNumber]
     except ValueError:
         print("Invalid clientID")
         return
@@ -149,74 +149,56 @@ def startGame(firstClientNumber, secondClientNumber):
 
         except socket.error:
             print("Client " + str(addr) + " disconnected from the server")
-            clients.remove(clientSocket)
+            activeSockets.remove(clientSocket)
             break
-
-
-# Keep track of all the clients messages
-clientLogs = []
-gameStarted = False
-
-
-class ClientLog:
-    def __init__(self, clientId, response):
-        self.clientId = clientId
-        self.response = response
 
 
 # This function handles the client connection, it sends the response to the client and closes the connection
 def handleClient(clientSocket, clientIP):
     print("Started new thread for client" + str(
-        clientIP) + linje)
-    clientID = getClientId(clientSocket)
+        clientIP) + line)
+    global clientLogs
     # Request log
     chatlog = []
     # Send welcome message to the client
     clientSocket.send("Welcome to the chat! Type 'help' for help. ".encode())
     while True:
         try:
-            # Get the request
-            clientRequest = clientSocket.recv(1024).decode()
-            # Start game
-            if clientRequest == "play":
-                break
-            # Check if the client wants to exit
-            if clientRequest == "exit":
-                print("Client " + str(clientIP) + " disconnected from the server")
-                clients.remove(clientSocket)
-                break
-            else:
-                clientRequest = clientRequest.replace('HelloServer', '')
-                if len(clientRequest) != 0:
-                    # Add the request to the logs
-                    chatlog.append(clientRequest)
-                    clientLogs.append(ClientLog(clientID, clientRequest))
-                    print("Client " + str(clientIP) + " sent a request: " + clientRequest)
-                    if "help" in clientRequest:
-                        clientSocket.send(helpClient().encode())
-                    elif "list" in clientRequest:
-                        clientSocket.send(listOfClients(clientSocket).encode())
-                    elif "msg" in clientRequest or "message" in clientRequest:
-                        # Find the client to send the message to
-                        toClient = clientRequest.split(" ")[1]
-                        text = ""
-                        # Get all the words after the clientID
-                        for i in range(2, len(clientRequest.split(" "))):
-                            if i == 2:
-                                text = clientRequest.split(" ")[i]
-                            else:
-                                text += " " + clientRequest.split(" ")[i]
-                        message(toClient, text)
-                    elif "game" in clientRequest:
-                        # Find the client to send the message to
-                        toClient = clientRequest.split(" ")[1]
-                        thread.start_new_thread(startGame, (getClientId(clientSocket), toClient))
+            # Get the request and remove the HelloServer from the request
+            request = formatRequest(clientSocket)
+            # request = clientSocket.recv(1024).decode().replace('HelloServer', '')
 
-                    elif "log" in clientRequest:
-                        clientSocket.send(str(chatlog).encode())
+            # If the request is not empty
+            if len(request) != 0:
+                # Add the request to the logs
+                chatlog.append(request)
+                clientLogs.append(ClientLog(clientSocket, request))
+                print("Client " + str(clientIP) + " sent a request: " + request)
+                if "help" == request:
+                    clientSocket.send(helpClient().encode())
+                elif "list" == request:
+                    clientSocket.send(listOfClients(clientSocket).encode())
+                elif "msg" == request or "message" == request:
+                    # Find the client to send the message to
+                    toClient = request.split(" ")[1]
+                    text = ""
+                    # Get all the words after the clientID
+                    for i in range(2, len(request.split(" "))):
+                        if i == 2:
+                            text = request.split(" ")[i]
+                        else:
+                            text += " " + request.split(" ")[i]
+                    message(toClient, text)
+                elif "game" in request:
+                    print("Game request")
+                    gamelobby.append(clientSocket)
+                elif "quit" == request:
+                    gamelobby.remove(clientSocket)
+                elif "log" in request:
+                    clientSocket.send(str(chatlog).encode())
         except socket.error:
             print("Client " + str(addr) + " disconnected from the server")
-            clients.remove(clientSocket)
+            activeSockets.remove(clientSocket)
             break
 
 
@@ -243,8 +225,8 @@ def startGameNew(username):
 
 
 def broadcast(clientSocket, clientIP):
-    if len(clients) > 1:
-        for client in clients:
+    if len(activeSockets) > 1:
+        for client in activeSockets:
             if client != clientSocket:
                 broadcastMessage = "New client  " + str(clientIP) + " connected to the server"
                 try:
@@ -254,36 +236,95 @@ def broadcast(clientSocket, clientIP):
                     print("Unable to send broacast message to " + str(client))
 
 
-# Check if any clients have disconnected
+# Check if any activeSockets have disconnected
 def checkForConnections():
-    print("Started new thread to check for disconnected clients" + linje)
+    print("Started new thread to check for disconnected activeSockets" + line)
+    global gamelobby
     while True:
         # Sleep for 2 seconds
         time.sleep(2)
-        for client in clients:
+        for client in activeSockets:
             try:
                 client.sendall("HelloClient".encode())
             except socket.error:
                 print("Client " + str(addr) + " disconnected from the server")
-                clients.remove(client)
-        if len(clients) == 0:
+                activeSockets.remove(client)
+        if len(activeSockets) == 0:
+            gamelobby = []
             break
-    print("No connections" + linje)
+    print("No connections" + line)
 
 
-# List of active clients
-clients = []
+def formatRequest(socket):
+    # Format the request remove HelloServer
+    request = socket.recv(1024).decode().replace('HelloServer', '')
+    if len(request) == 0:
+        return ""
+    return request
+
+
+# Check if any activeSockets have disconnected
+def checkLobby():
+    print("Started new thread to check for players seeking game" + line)
+    activeGames = []
+
+    while True:
+        # Sleep for 2 seconds
+        time.sleep(2)
+        for client in gamelobby:
+            try:
+                # print("Client " + str(client) + " is in the lobby")
+                # Get the messages from the players in the lobby
+                for msg in clientLogs:
+                    # If the socket is the same as the client in the lobby do stuff
+                    if msg.clientSocket == client:
+                        print(msg.request)
+                        clientLogs.remove(msg)
+                # Hvis det er to spillere i lobbyen, legg de til i activeGames,
+
+            except socket.error:
+                print("Client " + str(addr) + " disconnected from the server")
+                activeSockets.remove(client)
+                gamelobby.remove(client)
+        if len(gamelobby) == 0:
+            break
+    print("Lobby empty" + line)
+
+
+# List of active activeSockets
+activeSockets = []
+gamelobby = []
+
+# Keep track of all the client requests
+clientLogs = []
+
+
+class ClientLog:
+    def __init__(self, clientSocket, request):
+        self.clientSocket = clientSocket
+        self.request = request
+
+
+class Game:
+    def __init__(self, clientSocket1, clientSocket2, clientSocket1Points, clientSocket2Points):
+        self.clientSocket1 = clientSocket1
+        self.clientSocket2 = clientSocket2
+        self.clientSocket1Points = clientSocket1Points
+        self.clientSocket2Points = clientSocket2Points
+
 
 # This loop will run forever, accepting connections and serving creating a new thread for each connection
 while True:
+    thread.start_new_thread(checkLobby, ())
     # Accept a connections
     clientSocket, addr = serverSocket.accept()
-    # Add the client to the list of clients
-    clients.append(clientSocket)
+    # Add the client to the list of activeSockets
+    activeSockets.append(clientSocket)
     broadcast(clientSocket, addr)
     thread.start_new_thread(handleClient, (clientSocket, addr))
-    # Start a new thread to check for disconnected clients if not started
-    if len(clients) == 1:
+    # Start a new thread to check for disconnected activeSockets if not started
+
+    if len(activeSockets) == 1:
         thread.start_new_thread(checkForConnections, ())
 
 # Close the socket
